14f5f8ccbddad237e91d3ec0f4e80b233c892804
Anri Liy <amedamachip713@gmail.com>
2022-01-06 22:56:47 -0800

    Material UI

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Typography  from '@mui/material/Typography';
 
 const Nav = () => {
   return (
-    <nav>
-      NavMain
-    </nav>
+    <AppBar position="static">
+      <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+              Cruise
+      </Typography>
+    </AppBar>
   )
 }
 
