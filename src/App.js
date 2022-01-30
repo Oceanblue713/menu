e2b3aa77783c4372523d9e96e5b6235a632a53b1
Anri Liy <amedamachip713@gmail.com>
2022-01-30 10:21:49 -0800

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
+import Article from "./components/Articles/Article";
 import Footer from "./components/Footer";
-import {} from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <div className="app">
+        <Grid>
+          <Nav />
+
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="article" element={<Article />} />
+          </Routes>
+
+          <Footer />
+        </Grid>
+      </div>
+    </Router>
   );
 }
 
