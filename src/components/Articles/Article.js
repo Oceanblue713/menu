3de6cf9df9840835f4fd75b71bf1660fa2a15201
Anri Liy <amedamachip713@gmail.com>
2022-02-14 19:21:29 -0800

    Article Page

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ArticleContainer from './ArticleContainer';
-import { articles } from '../../articles/article1';
+import React from "react";
+import { Container, CardMedia, Typography } from "@mui/material";
+import classes from "./Article.module.css";
 
-const Article = () => {
-
-  const [articles, setArticles] = useState();
+const Article = (props) => {
   return (
-    <ArticleContainer title={} />
-  )
-}
+    <Container fixed disableGutters>
+      <CardMedia
+        component="img"
+        height="400"
+        width="100vw"
+        image={props.image}
+        alt=""
+        className="article-image "
+      />
+      <Typography variant="h3">{props.title}</Typography>
+      <Typography variant="body1" gutterBottom>
+        {props.article}
+      </Typography>
+    </Container>
+  );
+};
+
+export default Article;
