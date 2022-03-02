b92c494cb00d9051b22a856288ad80f54fead988
Anri Liy <amedamachip713@gmail.com>
2022-03-01 22:24:11 -0800

    Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
+import Article from "./components/Articles/Article";
 import ArticleContainer from "./components/Articles/ArticleContainer";
 import Footer from "./components/Footer";
 import Grid from "@mui/material/Grid";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
 
-function App() {
+function App(item) {
   return (
     <Router>
       <div className="app">
@@ -14,7 +20,7 @@ function App() {
           <Nav />
           <Routes>
             <Route path="/" exact element={<Main />} />
-            <Route path="/article/:link" element={<ArticleContainer />} />
+            <Route path="/article" component={<ArticleContainer />} />
           </Routes>
           <Footer />
         </Grid>
--- src/components/Articles/ArticleContainer.js
+++ src/components/Articles/ArticleContainer.js
@@ -3,20 +3,15 @@ import Article from "./Article";
 import { articles } from "../../articles/article1";
 
 const ArticleContainer = () => {
-  // <Article
-  //   title={articles[0].title_en}
-  //   article={articles[0].article_en}
-  //   image={articles[0].image}
-  //   link={articles[0].link}
-  // />
-  return articles.map((item) => {
-    <Article
-      title={item.title_en}
-      article={item.article_en}
-      image={item.image}
-      link={item.link}
-    />;
-  });
+  <Article img={articles[0].image} article={articles[0].article_en} />;
+  // return articles.map((item) => {
+  //   <Article
+  //     title={item.title_en}
+  //     article={item.article_en}
+  //     image={item.image}
+  //     link={item.link}
+  //   />;
+  // });
 };
 
 export default ArticleContainer;
