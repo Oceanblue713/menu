d2d5e5fc95dc57b58ca98985951574eb565d4a52
Anri Liy <amedamachip713@gmail.com>
2022-03-25 16:46:15 -0700

    Add New Article Page

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,20 @@ import React from "react";
 import CardContainer from "./Cards/CardContainer";
 import logo from "./Images/Cruisenewbie.png";
 
-const Main = (props) => {
+const Main = () => {
   return (
     <div id="main">
       <img src={logo} id="logo" />
-      <CardContainer />
+      <div className="main-intro">
+        We are cruisenewbie, introduce you to Royal Caribbean cruise for Newbies
+      </div>
+      <div className="main-cards">
+        <h2>Articles</h2>
+        <CardContainer />
+      </div>
+      <div className="main-garary">
+        <h2>Gallery</h2>
+      </div>
     </div>
   );
 };
