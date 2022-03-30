d97a53b3a8880239fb0db0d7f175806e3bf313b3
Anri Liy <amedamachip713@gmail.com>
2022-03-29 22:23:28 -0700

    Add Gallery

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+import { height } from "@mui/system";
 import React from "react";
 import CardContainer from "./Cards/CardContainer";
 import logo from "./Images/Cruisenewbie.png";
@@ -5,7 +6,14 @@ import logo from "./Images/Cruisenewbie.png";
 const Main = () => {
   return (
     <div id="main">
-      <img src={logo} id="logo" />
+      <div
+        className="main-logo"
+        style={{ backgroundImage: "url(/images/Beach.jpg)", height: "100%" }}
+      >
+        {" "}
+        <img src={logo} id="logo" />
+      </div>
+
       <div className="main-intro">
         We are cruisenewbie, introduce you to Royal Caribbean cruise for Newbies
       </div>
