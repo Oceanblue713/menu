d8e32db74cf073230ebf8f9971e9c78925cd209e
Anri Liy <amedamachip713@gmail.com>
2022-06-12 16:53:33 -0700

    Small Card

--- src/components/Cards/SmallCard.js
+++ src/components/Cards/SmallCard.js
@@ -1,19 +1,22 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import { CardMedia } from "@material-ui/core";
 
 const SmallCard = (props) => {
   return (
     <div className="smallcard">
-      <Card sx={{ display: "flex" }}>
-        <h5>{props.title}</h5>
-        <CardMedia
-          component="img"
-          image={props.image}
-          alt={props.title}
-          sx={{ width: 1 / 2, height: 1 / 2 }}
-        />
-      </Card>
+      <Box sx={{ maxWidth: 400 }}>
+        <Card>
+          <h5>{props.title}</h5>
+          <CardMedia
+            component="img"
+            image={props.image}
+            alt={props.title}
+            height={150}
+          />
+        </Card>
+      </Box>
     </div>
   );
 };
